a95ba4506eb52f3ec929760d18d337a3d56109d6
Mox Biggs <moxdev1@gmail.com>
2016-11-10 15:07:38 -0500

    setup sass and customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,6 +14,115 @@ function mm4_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+    $wp_customize->remove_section( 'colors' );
+
+    // Company Information section
+    $wp_customize->add_section ( 'company_contact_information', array(
+        'title'       => 'Company Information',
+        'description' => 'Fill out your company information',
+        'priority'    => '1'
+    ) );
+    // Company name
+    $wp_customize->add_setting ( 'setting_name', array(
+        'default'           => 'Portabello Apartments',
+        'sanitize_callback' => 'sanitize_text'
+    ) );
+    $wp_customize->add_control ( 'setting_name', array(
+        'label'   => 'Company Name',
+        'section' => 'company_contact_information',
+        'type'    => 'text'
+    ) );
+    // Company address
+    $wp_customize->add_setting ( 'setting_address', array(
+        'sanitize_callback' => 'sanitize_text'
+    ) );
+    $wp_customize->add_control ( 'setting_address', array(
+        'label'   => 'Street Address',
+        'section' => 'company_contact_information',
+        'type'    => 'text'
+    ) );
+    // City
+    $wp_customize->add_setting ( 'setting_city', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_city', array(
+            'label'   => 'City',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // State
+    $wp_customize->add_setting ( 'setting_state', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_state', array(
+            'label'   => 'State',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // Company zipcode
+    $wp_customize->add_setting ( 'setting_zipcode', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_zipcode', array(
+            'label'   => 'Zip Code',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // Company phone number
+    $wp_customize->add_setting ( 'setting_phone', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_phone', array(
+            'label'   => 'Phone Number',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // Fax number
+    $wp_customize->add_setting ( 'setting_fax', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_fax', array(
+            'label'   => 'Fax Number',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // Email
+    $wp_customize->add_setting ( 'setting_email', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_email', array(
+            'label'   => 'Email',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // Office Hours 1
+    $wp_customize->add_setting ( 'setting_hours1', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_hours1', array(
+            'label'   => 'Office Hours 1',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // Office Hours 2
+    $wp_customize->add_setting ( 'setting_hours2', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_hours2', array(
+            'label'   => 'Office Hours 2',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
+    // Office Hours 3
+    $wp_customize->add_setting ( 'setting_hours3', array(
+            'sanitize_callback' => 'sanitize_text'
+        ) );
+    $wp_customize->add_control ( 'setting_hours3', array(
+            'label'   => 'Office Hours 3',
+            'section' => 'company_contact_information',
+            'type'    => 'text'
+        ) );
 }
 add_action( 'customize_register', 'mm4_customize_register' );
 
