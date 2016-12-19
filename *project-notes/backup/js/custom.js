fdb83e2d563facc4d49c4c7bcfecb49fcbe18356
Mox Biggs <moxdev1@gmail.com>
2016-12-19 17:05:38 -0500

    cleanup

--- *project-notes/backup/js/custom.js
+++
@@ -1,8 +0,0 @@
-/*
-* Loads the Flexslider JS for the Home Page Image Carousel
- */
-(function($) {
-  $(window).load(function() {
-
-  });
-})(jQuery);
--- *project-notes/backup/js/min/custom-min.js
+++
@@ -1,1 +0,0 @@
-!function($){$(window).load(function(){})}(jQuery);
